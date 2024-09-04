e6324d1d06fdbd07435b766148dac97a1ff0e2b9
Nandita Raj Kumar <nanditark@Nanditas-MacBook-Pro.local>
2024-09-03 17:15:23 -0700

    added navbar changes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,41 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from '../../../public/assets/logo.png'
-
+import menu from '../../../public/assets/menu.webp'
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+
   return (
+    <>
     <ul className='flex justify-between'>
 
         <Link href='/'><li><Image src={logo} width={100} height={100} alt='Homeless Help Logo'/></li></Link>
-        <div className='flex w-1/2 items-center text-lg justify-around'>
+        <div className='flex mx-8 justify-center items-center md:hidden'>
+          <Image onClick={()=>setToggle(!toggle)} src={menu} width={25} height={25} alt='Menu button' />
+        </div>
+        <div className='hidden md:flex w-1/2 items-center text-lg justify-around'>
             <Link className='duration-500  hover:border hover:border-b-2 hover:border-b-main border-white' href='/shelter'><li>Shelter</li></Link>
             <Link className='duration-500  hover:border hover:border-b-2 hover:border-b-main border-white' href='/food'><li>Food</li></Link>
             <Link className='duration-500  hover:border hover:border-b-2 hover:border-b-main border-white' href='/hygiene'><li>Hygiene</li></Link>
             <Link className='duration-500  hover:border hover:border-b-2 hover:border-b-main border-white' href='/health'><li>Health</li></Link>
         </div>
+
+        
         
 
     </ul>
+    {toggle && <div className='duration-500 absolute flex flex-col z-10 bg-main text-white w-full list-none items-center text-lg justify-around'>
+      <Link className='p-4 w-full text-center hover:bg-black hover:text-white' href='/shelter'><li>Shelter</li></Link>
+      <Link className='p-4 w-full text-center hover:bg-black hover:text-white' href='/food'><li>Food</li></Link>
+      <Link className='p-4 w-full text-center hover:bg-black hover:text-white' href='/hygiene'><li>Hygiene</li></Link>
+      <Link className='p-4 w-full text-center hover:bg-black hover:text-white' href='/health'><li>Health</li></Link>
+  </div>}
+
+  </>
+    
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,18 @@ export default function Home() {
       <Navbar />
       
       <div className="flex h-[80vh]">
-        <div className="w-1/3 h-full">
-        <Image className=" m-24" src={pattern} width={300} height={200} alt="Pattern"/>
+        <div className="lg:w-1/3 h-full">
+          <Image className="hidden z-0 lg:flex m-24" src={pattern} width={300} height={200} alt="Pattern"/>
 
         </div>
-       <div className="w-1/3 h-full flex flex-col items-center justify-center">
-       <h1 className="text-center text-main text-6xl m-8">Homeless Help</h1>
-       <p className="m-8 text-lg text-center">Some description goes here or a catchy catchphrase goes here.</p>
-       <Link href='/getstarted'><button className="bg-black text-white animate-bounce p-6 px-12 m-8 rounded-full">Get Started → </button></Link>
+       <div className="lg:w-1/3 mx-auto h-full flex flex-col items-center justify-center">
+       <h1 className="text-center text-main text-6xl m-8 p-4">Homeless Help</h1>
+       <p className="m-8 text-lg text-center">Providing essentials and improving lives one at a time.</p>
+       <Link href='/getstarted'><button className="bg-black text-white animate-bounce w-fit p-6 md:px-12 m-8 rounded-full">Get Started → </button></Link>
 
        </div>
-        <div className="w-1/3 h-full flex items-end justify-end ">
-        <Image className=" m-24" src={pattern} width={300} height={200} alt="Pattern" />
+        <div className="lg:w-1/3 hidden h-full lg:flex lg:items-end lg:justify-end ">
+        <Image className="hidden lg:flex lg:m-24" src={pattern} width={300} height={200} alt="Pattern" />
 
         </div>
        
